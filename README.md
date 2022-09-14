a8eacba14f7e07b55bb46ff8899a92324dd4d235
Chen-Yuan Chu <cchu42@myseneca.ca>
2022-09-14 08:35:14 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+<h3 align="center">Txt-to-HTML Static Site Converter</h3>
+
+## Goal
+This is mainly a tool that covnerts txt files to static web pages.
+
+## Usage
+The user can provide one or more txt files to covnert into html file(s) of the same names.
+
+## Features
+WIP
---
+++ staticSiteGen.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const readline = require("readline");
+
+function txtReader(txt){
+    const source = txt;//the source txt file
+    const destination = txt.replace('txt', 'html');//have the new file be html but keep the name
+    const inStream = fs.createReadStream(source);
+    const outStream = fs.createWriteStream(destination, { encoding: "utf8" });
+
+    //interface
+    var reader = readline.createInterface({
+      input: inStream,
+      terminal: false
+    });
+
+    //change to each line
+    reader.on("line", function(line) {
+     if(line.trim()==""){
+        outStream.write(line);
+     }
+     else{
+        outStream.write("<p>"+line+"</p>\n");
+     }
+      
+    });
+}
+
+txtReader("input.txt");
+//make it accept single or multiple files(folder)
+//consider omit the soruce = txt step
